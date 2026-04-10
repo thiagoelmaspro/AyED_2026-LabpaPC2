2d3d83738a527d2999853133a0b396e939faa154
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-10 17:03:44 -0300

    Add files via upload

---
+++ condicionales primera parte/tp1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime la velocidad de un vehiculo : ");
+            int velocidad = Convert.ToInt32(Console.ReadLine());
+
+            if (velocidad > 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else
+            {
+                Console.WriteLine("Conduccion segura");
+            }
+
+        }
+    }
+}
