300e6770cbb89108cff6fea05b7e8af8a07722cb
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 15:18:03 -0300

    Add files via upload

---
+++ condicionales primera parte/tp5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, decime cuanto vale esto; ");
+            Double monto = Convert.ToInt32(Console.ReadLine());
+            Double original = monto;
+            Double descuento = monto / 0.15;
+            Double total = monto - descuento;
+            if (monto > 5000)
+            {
+
+                Console.WriteLine("Se aplico el descuento, el total es " + descuento);
+               
+
+            }
+            else
+               
+              Console.WriteLine("No se aplicaron descuentos. El precio final es " + original);
+
+        }
+    }
+}
