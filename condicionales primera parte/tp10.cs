4737873f7e65dba0c9a3c6a69fc93a1e77e31339
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 16:10:49 -0300

    Add files via upload

---
+++ condicionales primera parte/tp10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime el presupuesto disponible: ");
+            int presupuesto = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Decime el costo: ");
+            int costo = Convert.ToInt32(Console.ReadLine());
+            int diferencia = presupuesto - costo;
+
+            if (presupuesto > costo)          
+            {
+
+                Console.WriteLine("El presupuesto alcanza");
+            }
+            else
+                Console.WriteLine("El costo es mas, faltaria un total de " + diferencia);
+        }
+    }
+}
