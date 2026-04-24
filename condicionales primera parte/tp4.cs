0dbc9204aa7db0d795111ba205a55e1717ac52b8
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 14:32:09 -0300

    Add files via upload

---
+++ condicionales primera parte/tp4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime una nota de un examen: ");
+            double nota = Convert.ToInt32(Console.ReadLine());
+
+            if (nota > 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else if (nota > 4)
+            {
+                Console.WriteLine("A finales");
+            }
+            else
+                Console.WriteLine("Recuperatorio");
+        }
+    }
+}
