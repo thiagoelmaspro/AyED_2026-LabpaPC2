ba4b6342f151982df219b0264d280c0842a4f1bf
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 16:45:17 -0300

    Add files via upload

---
+++ condicionales t2/12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime el peso: ");
+            Double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Decime la altura: ");
+            Double altura = Convert.ToDouble(Console.ReadLine());
+            Double IMC = peso / (altura * altura);
+
+            if( IMC > 25)
+            {
+                Console.WriteLine("Sobrepeso ");
+
+                
+            }
+           else
+            {
+                Console.WriteLine("Rango normal");
+            }
+        }
+    }
+}
