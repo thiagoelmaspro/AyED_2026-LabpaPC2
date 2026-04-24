f85b5e7350e9e20f096b2c991a5c7f646a43d6bc
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 16:33:53 -0300

    Add files via upload

---
+++ condicionales t2/tp11.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime un numero: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Decime otro numero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Decime el ultimo numero: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2 && num2 == num3)
+            {
+                Console.WriteLine("El triangulo es Equilatero ");
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("Es un triangulo Isóceles");
+            }
+            else if (num1 == num3)
+            {
+                Console.WriteLine("Es un triangulo Isóceles");
+            }
+            else if (num2 == num1)
+            {
+                Console.WriteLine("Es un triangulo Isóceles");
+
+            }
+            else if (num3 == num2)
+            {
+                Console.WriteLine("Es un triangulo Isóceles");
+            }
+            else
+            {
+                    Console.WriteLine("Es Escaleno");
+            }
+
+             
+        }
+    }
+}
