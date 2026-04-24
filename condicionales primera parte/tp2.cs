67cc454be214089dfc306b690a36621339763e66
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 14:14:44 -0300

    Add files via upload

---
+++ condicionales primera parte/tp2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Hola Como estas?: ");
+            Console.WriteLine("Cuantos años tenes?: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Bienvenido al club de pelea ");
+
+            } 
+            else
+            {
+                Console.WriteLine("Lo siento, eres muy polluelo para esto ");
+
+            }   
+
+
+
+
+
+        }
+    }
+}
