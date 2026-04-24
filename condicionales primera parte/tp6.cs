2ea8801c70837d4055b9682ac6d04c834bbebbd1
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 15:33:54 -0300

    Add files via upload

---
+++ condicionales primera parte/tp6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime tu nombre de usurio: ");
+            string usuario = Console.ReadLine();
+            Console.WriteLine("Decime la clave del usuario");
+            string clave = Console.ReadLine();
+
+            if (usuario == "admin" && clave == "1234")
+            {
+                Console.WriteLine("Acceso concedido ");
+            }
+            else
+                Console.WriteLine("Error de credenciales");
+        }
+
+        
+    }
+}
