4b4c41cebae64239ba9f9776c43c82686e9351a3
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 15:54:07 -0300

    Add files via upload

---
+++ condicionales primera parte/tp7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime la temperatura ");
+            float temperatura = Convert.ToInt32(Console.ReadLine());
+            if (temperatura < 0)
+            {
+                Console.WriteLine("Peligro de congelamiento ");
+
+            }
+            else if (temperatura <= 15)
+            {
+                Console.WriteLine("Mucho frio ");
+
+            }
+            else
+                Console.WriteLine("Clima agradable ");
+        }
+    }
+}
