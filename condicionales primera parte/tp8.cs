b9dad524e5b261fd062463d609616a730a109eb7
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 15:47:34 -0300

    Add files via upload

---
+++ condicionales primera parte/tp8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime un numero ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 5 == 0)
+            {
+                Console.WriteLine("Si, es divisible por 5 ");
+            }
+            else
+                Console.WriteLine("No, no es divisible");
+
+        }
+    }
+}
