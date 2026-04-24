cd9c9848b45b03f2b66fb01cc7a7e0bbfb7c8597
Tsairaf <thiagofariasx3212@gmail.com>
2026-04-24 14:22:42 -0300

    Add files via upload

---
+++ condicionales primera parte/tp3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, decime un numero entero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par ");
+
+
+            }
+            else
+                Console.WriteLine("Es impar");
+        }
+
+    }
+}
