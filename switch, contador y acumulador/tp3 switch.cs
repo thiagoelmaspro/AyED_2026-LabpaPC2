014d0db0c238c766c2f598376c97374311194300
Tsairaf <thiagofariasx3212@gmail.com>
2026-05-08 16:40:02 -0300

    Add files via upload

---
+++ switch, contador y acumulador/tp3 switch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ahorroacumulado = 0;
+            int ahorro = 0;
+           
+            for (int i=1 ; i <=4 ; i++ )
+            {
+                Console.WriteLine("Ingrese el ahorro de la semana N° " + i + ":");
+                ahorro = int.Parse(Console.ReadLine());
+
+                ahorroacumulado = ahorroacumulado + ahorro;
+
+                Console.WriteLine("El dinero acumulado del mes es de " + ahorroacumulado);
+            }
+            
+           
+        
+        }
+    }
+}
---
+++ switch, contador y acumulador/tp4 switch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+            for (int i=1 ; i<=5 ; i++)
+            {
+                Console.WriteLine("Ingresa un numero: ");
+                int numero = int.Parse(Console.ReadLine());
+                
+                if (numero > 100)
+                {
+                    
+                    contador++;
+
+                }
+                
+
+            }
+
+            Console.WriteLine("Hay " + contador + " numeros mayores");
+
+        }
+    }
+}
