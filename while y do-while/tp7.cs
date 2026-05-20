ab82a35444e009c401df4b4f6b9e3beae869efce
Tsairaf <thiagofariasx3212@gmail.com>
2026-05-20 17:02:24 -0300

    Add files via upload

---
+++ while y do-while/tp7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            int suma = 0;
+            int contador = 1;
+
+            while (contador <= 5) ;
+
+            Console.WriteLine("Ingrese la nota del alumno: ");
+            nota = int.Parse(Console.ReadLine());
+            suma = suma + nota;
+            contador ++;
+
+        }
+        
+
+     
+    }
+}
