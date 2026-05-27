9e476e414c530455b8734f64749f442c7922ed38
Tsairaf <thiagofariasx3212@gmail.com>
2026-05-27 15:38:26 -0300

    Add files via upload

---
+++ while y do-while/tp10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int resultado = 0;
+            int contador = 0;
+
+            Console.WriteLine("Ingrese un numero: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero: ");
+            b = int.Parse(Console.ReadLine());
+
+
+
+            while (contador < b)
+            {
+                
+                resultado = resultado + a;
+                contador = contador + 1;
+                
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
