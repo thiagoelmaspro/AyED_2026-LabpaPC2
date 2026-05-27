1cd1479d40d88ac3080f7b9d699136fd58263117
Tsairaf <thiagofariasx3212@gmail.com>
2026-05-27 14:12:15 -0300

    Add files via upload

---
+++ while y do-while/tp8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura;
+
+            Console.Write("Ingrese la temperatura inicial: ");
+            temperatura = int.Parse(Console.ReadLine());
+
+            do
+            {
+                temperatura = temperatura - 2;
+                Console.WriteLine("Enfriando... Temperatura actual: " + temperatura + "°C");
+
+            } while (temperatura > 24);
+
+            Console.WriteLine("Temperatura ideal alcanzada");
+
+        }
+    }
+}
