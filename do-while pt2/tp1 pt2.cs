4244abd40aa652b83466a1fd5f48ed8cf250bc6a
Tsairaf <thiagofariasx3212@gmail.com>
2026-05-27 16:33:34 -0300

    Add files via upload

---
+++ do-while pt2/tp1 pt2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int saldo = 10000;
+            int opcion = 0;
+            int dinero;
+
+            while (opcion != 4)
+            {
+                Console.WriteLine("CAJERO AUTOMATICO ");
+                Console.WriteLine("1-Depositar dinero ");
+                Console.WriteLine("2-Retirar dinero ");
+                Console.WriteLine("3-Ver saldo actual ");
+                Console.WriteLine("4-Salir ");
+                Console.Write("Elija una opcion: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+
+                {
+                    case 1:
+
+                        Console.Write("Ingrese el dinero a depositar ");
+                        dinero = int.Parse(Console.ReadLine());
+                        saldo = saldo + dinero;
+                        Console.WriteLine("Deposito realizado");
+                        break;
+
+                    case 2:
+
+
+                        Console.WriteLine("Ingrese el dinero a retirar ");
+                        dinero = int.Parse(Console.ReadLine());
+                        saldo = saldo - dinero;
+                        Console.WriteLine("Dinero retirado con exito ");
+                        break;
+
+
+
+
+                    case 3:
+                        Console.WriteLine("El dinero actual es de $" + saldo );
+                        break;
+
+
+
+                    case 4:
+                        Console.WriteLine("Saliendo del cajero... ");
+                        break;
+
+
+
+
+
+
+                }
+
+
+
+
+            }
+
+
+
+            
+        }
+    }
+}
