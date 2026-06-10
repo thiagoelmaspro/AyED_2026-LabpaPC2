d93280f8a0c3fb569f5973b5b4a5d4c5067c5f0d
Tsairaf <thiagofariasx3212@gmail.com>
2026-06-10 17:02:44 -0300

    Add files via upload

---
+++ do-while pt2/tp2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secreto = 42;
+            int numero;
+            int intentos = 0;
+
+            while (intentos < 5)
+            {
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+                intentos++;
+
+                if ( numero == secreto )
+                {
+                    Console.WriteLine("¡¡GANASTEE!! ");
+                    Console.WriteLine("Intentos usados: " + intentos);
+
+                }
+              else if ( numero < secreto)
+                {
+                    Console.WriteLine("El numero es mayor ");
+                }
+                else
+                {
+                    Console.WriteLine("El numero es menor");
+                }
+                
+            }
+        }
+    }
+}
