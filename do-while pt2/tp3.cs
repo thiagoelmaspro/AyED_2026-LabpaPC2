bad083e84f0e3130d3f7ccc83e7bcca5b6b2afda
Tsairaf <thiagofariasx3212@gmail.com>
2026-06-10 17:03:06 -0300

    Add files via upload

---
+++ do-while pt2/tp3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double venta;
+            
+            double total = 0;
+            double mayor = 0;
+
+            int cantidad = 0;
+
+            string respuesta;
+
+            do
+            {
+                Console.WriteLine("Ingrese el monto de la venta: ");
+                venta = double.Parse(Console.ReadLine())
+                    ;
+
+                total = total + venta;
+                cantidad++;
+
+                if (venta > mayor)
+                {
+                    mayor = venta;
+
+                }
+                Console.WriteLine("¿Desea ingresar otra venta?: ");
+                respuesta = Console.ReadLine();
+
+
+                
+            } while (respuesta == "S");
+
+            Console.WriteLine("Total recaudado: " + total);
+            Console.WriteLine("Cantidad de ventas: " + cantidad);
+            Console.WriteLine("Venta mas alta:: " + mayor);
+        }
+    }
+}
