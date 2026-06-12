2e865f0d9060f6b944fdb3accda21106ebefb0cf
Tsairaf <thiagofariasx3212@gmail.com>
2026-06-12 14:26:17 -0300

    Add files via upload

---
+++ do-while pt2/tp4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string usuario;
+        string contraseña;
+        int intentos = 0;
+
+        do
+        {
+            Console.Write("Usuario: ");
+            usuario = Console.ReadLine();
+
+            Console.Write("Contraseña: ");
+            contraseña = Console.ReadLine();
+
+            intentos++;
+
+        } while ((usuario != "admin" || contraseña != "1234") && intentos < 3);
+
+        if (usuario == "admin" && contraseña == "1234")
+        {
+            Console.WriteLine("Bienvenido al sistema");
+        }
+        else
+        {
+            Console.WriteLine("Cuenta bloqueada por seguridad");
+        }
+    }
+}
