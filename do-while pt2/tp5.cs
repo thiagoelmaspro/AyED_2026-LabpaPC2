80f27f83b7b87ff91c50712d2b9fbb3ebb0d33f4
Tsairaf <thiagofariasx3212@gmail.com>
2026-06-12 14:28:20 -0300

    Add files via upload

---
+++ do-while pt2/tp5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string dato;
+        int numero;
+
+        int positivos = 0;
+        int negativos = 0;
+        int ceros = 0;
+
+        do
+        {
+            Console.Write("Ingrese un número (o X para terminar): ");
+            dato = Console.ReadLine();
+
+            if (dato != "X" && dato != "x")
+            {
+                numero = Convert.ToInt32(dato);
+
+                if (numero > 0)
+                {
+                    positivos++;
+                }
+                else
+                {
+                    if (numero < 0)
+                    {
+                        negativos++;
+                    }
+                    else
+                    {
+                        ceros++;
+                    }
+                }
+            }
+
+        } while (dato != "X" && dato != "x");
+
+        Console.WriteLine("Positivos: " + positivos);
+        Console.WriteLine("Negativos: " + negativos);
+        Console.WriteLine("Ceros: " + ceros);
+    }
+}
