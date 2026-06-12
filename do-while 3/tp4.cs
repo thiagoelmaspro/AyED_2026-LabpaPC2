7a54c9a4b9680ea389afb1ad4c1e3c6b0eee95d2
Tsairaf <thiagofariasx3212@gmail.com>
2026-06-12 16:26:40 -0300

    Add files via upload

---
+++ do-while 3/tp4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int menor = 0;
+
+        Console.Write("Ingrese un número: ");
+        numero = int.Parse(Console.ReadLine());
+
+        if (numero >= 0)
+        {
+            menor = numero;
+        }
+
+        while (numero >= 0)
+        {
+            if (numero < menor)
+            {
+                menor = numero;
+            }
+
+            Console.Write("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("El número menor es: " + menor);
+    }
+}
